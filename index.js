ee23f98d598177628c90220f3083b5c564ce21a9
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2024-02-03 21:44:55 -0600

    minor fixes

--- index.js
+++ index.js
@@ -19,11 +19,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
-});
-
 app.get("/api/whoami", (req, res) => {
   res.json({
     ipaddress: req.socket.remoteAddress,
